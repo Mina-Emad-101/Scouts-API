d9c84438b7c7a98f478b193b55a315880908b465
Mina Emad <menamanos@gmail.com>
2024-10-13 10:26:08 +0300

    static frontend

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import Express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import path from "path";
 import attendanceRouter from "./routes/v1/attendance.js";
 import scoutsRouter from "./routes/v1/scouts.js";
 
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || "127.0.0.1";
+const NODE_ENV = process.env.NODE_ENV || "development";
 const app = Express();
 
 // Middlewares
 app.use(cors());
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
+app.use(Express.static(path.join(import.meta.dirname, "public")));
 
 mongoose
   .connect(
-    process.env.NODE_ENV === "development"
+    NODE_ENV === "development"
       ? "mongodb://localhost:27017/scouts"
       : `mongodb+srv://mina:${process.env.DBPASSWORD}@cluster0.yjr3f.mongodb.net/scouts?retryWrites=true&w=majority&appName=Cluster0`,
   )
