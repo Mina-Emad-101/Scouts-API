1ecf846cb64734a74ddc274793c0b86ca6cceb62
Mina Emad <menamanos@gmail.com>
2024-10-13 22:38:56 +0300

    bug fix

---
+++ src/public/scripts.js
@@ -0,0 +1,43 @@
+async function signAttendance() {
+	const trash = document.getElementsByClassName("box")[0];
+	const brs = document.body.getElementsByTagName("br");
+	if (trash) {
+		trash.remove();
+		brs[brs.length - 1].remove();
+	}
+
+	const result = await fetch(
+		"https://scouts-api.onrender.com/api/v1/attendance",
+		{
+			method: "POST",
+			body: JSON.stringify({
+				scout_id: document.getElementById("scout_id").value,
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		},
+	).catch((err) => console.log(err));
+
+	document.body.appendChild(document.createElement("br"));
+	const div = document.createElement("div");
+	div.classList.add("box");
+	const p = document.createElement("p");
+	p.style.margin = "8px";
+	div.appendChild(p);
+	document.body.appendChild(div);
+
+	if (result.status === 200) {
+		div.classList.add("success_box");
+		p.innerHTML = "Signed Successfully";
+	} else if (result.status === 404) {
+		div.classList.add("error_box");
+		p.innerHTML = "ID Not Found";
+	} else if (result.status === 403) {
+		div.classList.add("error_box");
+		p.innerHTML = "Already Signed";
+	} else {
+		div.classList.add("error_box");
+		p.innerHTML = "An Error Occured";
+	}
+}
