5ccb0b21d06ffaa096f0d84486737bf512431fb9
Mina Emad <menamanos@gmail.com>
2024-11-09 01:25:56 +0200

    Main Page

--- src/routes/v1/front.js
+++ src/routes/v1/front.js
@@ -5,36 +5,37 @@ import attendanceSheets from "../../views/attendance-sheets.js";
 import createScout from "../../views/create-scout.js";
 import scouts from "../../views/scouts.js";
 import makeException from "../../views/make-exception.js";
+import main from "../../views/main.js";
 
 const router = Express.Router();
 
 router.get("/", (req, res) => {
-  return res.send();
+	return res.send(main());
 });
 
 router.get("/attendance", (req, res) => {
-  return res.send(attendanceSheets());
+	return res.send(attendanceSheets());
 });
 
 router.get("/attendance/:id", (req, res) => {
-  const id = req.params.id;
-  return res.send(getAttendance(id));
+	const id = req.params.id;
+	return res.send(getAttendance(id));
 });
 
 router.get("/sign-attendance", (req, res) => {
-  return res.send(signAttendance());
+	return res.send(signAttendance());
 });
 
 router.get("/create-scout", (req, res) => {
-  return res.send(createScout());
+	return res.send(createScout());
 });
 
 router.get("/scouts", (req, res) => {
-  return res.send(scouts());
+	return res.send(scouts());
 });
 
 router.get("/make-exception", (req, res) => {
-  return res.send(makeException());
+	return res.send(makeException());
 });
 
 export default router;
